e2027a7d5da2ffd3f195186dc1feaec67ebd41b0
Amine_88 <amin.ouni@neopolis-dev.com>
2023-10-19 10:05:29 +0100

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h4>React Init Project</h4>
+    </div>
+  );
+}
+
+export default App;
